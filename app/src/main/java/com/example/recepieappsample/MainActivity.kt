b229b0f0ca832f6e4cbbccd80a6ec9b8960146e5
Amirhossein Hamidian <amirhossein611@gmail.com>
2022-03-26 23:14:58 +0430

    column

--- app/src/main/java/com/example/recepieappsample/MainActivity.kt
+++ app/src/main/java/com/example/recepieappsample/MainActivity.kt
@@ -3,33 +3,72 @@ package com.example.recepieappsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.recepieappsample.ui.theme.RecepieAppSampleTheme
+import com.example.recepieappsample.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           Column(
-               modifier = Modifier.padding(16.dp)
-           ) {
-               Text(text = "hello compose")
-               Spacer(modifier = Modifier.padding(top = 10.dp))
-               Button(onClick = {}) {
-                   Text(text = "Button")
-               }
-           }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
+                    .background(color = Color(0xfff2f2f2))
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.happy_meal),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .height(300.dp)
+                        .fillMaxWidth()
+                )
+
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.End
+                ) {
+                    Text(
+                        text = "پکیج رنسانس",
+                        style = Typography.h5
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(
+                        text = "808 کالری",
+                        style = Typography.body2
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(
+                        text = "125,000 تومان",
+                        fontSize = 17.sp,
+                        color = Color(0xff85bb65)
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                }
+            }
         }
     }
 }
